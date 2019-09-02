d39036c15e9b45637f10bc47559b79b947a4ad20
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-09-03 00:28:17 +0530

    malayalam word vectors updated and loaded in local machine

---
+++ Tests/loadmodel.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  3 00:16:46 2019
+
+@author: abhijithneilabraham
+"""
+
+import io
+
+def load_vectors(fname):
+    fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
+    #n, d = map(int, fin.readline().split())
+    data = {}
+    for line in fin:
+        tokens = line.rstrip().split(' ')
+        data[tokens[0]] = map(float, tokens[1:])
+    return data
+print(load_vectors("/Users/abhijithneilabraham/Downloads/useful_stuff/mal.gz"))
