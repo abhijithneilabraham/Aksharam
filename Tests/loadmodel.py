b4bc461a07a0fb66769fdc4ffce7d15fc5e9201b
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-09-03 18:37:02 +0530

    Update Tests/loadmodel.py

--- Tests/loadmodel.py
+++ Tests/loadmodel.py
@@ -10,10 +10,11 @@ import io
 
 def load_vectors(fname):
     fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
-    #n, d = map(int, fin.readline().split())
-    data = {}
-    for line in fin:
-        tokens = line.rstrip().split(' ')
-        data[tokens[0]] = map(float, tokens[1:])
-    return data
-print(load_vectors("/Users/abhijithneilabraham/Downloads/useful_stuff/mal.gz"))
+    n, d = map(int, fin.readline().split())
+#    data = {}
+#    for line in fin:
+#        tokens = line.rstrip().split(' ')
+#        data[tokens[0]] = map(float, tokens[1:])
+#    return data
+    print(d)
+embed=load_vectors('/Users/abhijithneilabraham/Downloads/useful_stuff/mal')
